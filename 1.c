61feaac32c2e507c70cf04328b0beaf958a8b029
ManishNegi825 <108972949+ManishNegi825@users.noreply.github.com>
2022-07-18 15:27:26 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,12 @@
+/*Write a program to print MySirG 5 times on the screen*/
+
+#include<stdio.h>
+int main ()
+{
+    int i=1;
+    for(i=1;i<=5;i++)
+    {
+        printf("\nMySirG");
+    }
+    return 0;
+}
